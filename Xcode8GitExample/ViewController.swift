2dc101e36eb02ab1d8340abf910dbcc015b1ee24
Michael Ison <Argonath@michaels-imac-2.local>
2017-05-11 23:40:31 +1000

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Michael Ison on 11/5/17.
+//  Copyright © 2017 Michael Ison. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
